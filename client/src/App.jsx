a13f6f6b42a0a2b40be24a461d0abda557e68d55
CCBaxter84 <troyceclark@gmail.com>
2021-01-07 13:58:50 -0700

    implemented working BPI Chart

---
+++ client/src/App.jsx
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react';
+import Chart from 'chart.js';
+import axios from 'axios';
+
+function App() {
+  const [ dates, setDates ] = useState([]);
+  const [ bitcoinPrice, setBitcoinPrice ] = useState([]);
+
+  useEffect(() => {
+    getData();
+    createChart();
+  }, [dates]);
+
+  const getData = async () => {
+    try {
+      const res = await axios.get('https://api.coindesk.com/v1/bpi/historical/close.json');
+      setDates(Object.keys(res.data.bpi));
+      setBitcoinPrice(Object.values(res.data.bpi));
+    } catch(error) {
+      console.log(error);
+    }
+  };
+
+  const createChart = () => {
+    const ctx = document.getElementById('myChart');
+    new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: dates,
+        datasets: [
+          {
+            label: 'Bitcoint Price Index',
+            data: bitcoinPrice
+          }
+        ]
+      }
+    })
+  };
+
+  return (
+    <div className='App'>
+      <canvas id='myChart' width='400' height='400' />
+    </div>
+  );
+};
+
+export default App;
